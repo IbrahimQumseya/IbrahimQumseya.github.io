dc7d32f48e9cc27637dbeca48c42a844182570b2
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2019-11-22 14:53:39 +0200

    Add files via upload

---
+++ HTML/geo/geo.js
@@ -0,0 +1,18 @@
+document.getElementById("id_logic").innerHTML = "logic version = 2019.11.22.1";
+document.getElementById("id_button").addEventListener("click",start);
+var geo = navigator.geolocation;
+
+function start()
+{
+	geo.getCurrentPosition(on_ok_geo_uab,on_fail_geo_uab);
+}
+
+function on_ok_geo_uab(e){
+	document.getElementById("geo-location").innerHTML = e.latitude +" "+e.coords+" "+ e.longtitude + " " e.coords.accuracy;
+	
+}
+
+function on_fail_geo_uab(e){
+	alert("Failed" + e);
+}
+
