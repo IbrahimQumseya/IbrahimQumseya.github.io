31f81bc6c95935d7b2bf14d961d055e1996f11b8
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2019-11-22 13:24:40 +0200

    fixing

--- HTML/worker/desinare.js
+++ HTML/worker/desinare.js
@@ -33,6 +33,8 @@ function start()
 		muncitor.onmessage = function (e){
 			document.getElementById("id_prime").innerHTML = e.data;
 		}
+		
+		muncitor.postMessage("start");
 	}else{
 		muncitor.postMessage("start");
 		
