92cd9c2eadcb889183180e56afe726d91bc6827d
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2019-11-22 14:33:41 +0200

    Add files via upload

---
+++ HTML/cam/cam-js.js
@@ -0,0 +1,15 @@
+document.getElementById("id_logic").innerHTML = "logic version = 2019.11.22.1";
+var video = document.getElementById("id_video");
+var c =	{ audio: true, video: { facingMode: { exact: "environment" } } };
+
+navigator.mediaDevices.getUserMedia(c).then(on_ok_cam_uab).catch(on_fail_cam_uab);
+
+
+function on_ok_cam_uab(e){
+	video.srcObject= e;
+	
+}
+
+function on_fail_cam_uab(e){
+	alert("Failed" + e);
+}
