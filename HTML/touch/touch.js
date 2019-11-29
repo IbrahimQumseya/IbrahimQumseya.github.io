87789dfc595b1be65e4137ee5e2a7fca29fde5a5
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2019-11-29 13:30:04 +0200

    Add files via upload

--- HTML/touch/touch.js
+++ HTML/touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML ="Logic version = 2019.11.29.2";
+document.getElementById("id_logic").innerHTML ="Logic version = 2019.11.29.3";
 window.addEventListener("touchstart",touch_start_uab);
 
 var canvas = document.getElementById("id_canvas");
@@ -27,6 +27,7 @@ function touch_start_uab(e)
 		context.arc(t[i].pageX - client_rect.left,t[i].pageY - client_rect.top,10,0,2*Math.PI);
 		context.fillStyle = get_randomColor();
 		context.strokeStyle = context.fillStyle;
+		context.fill();
 		context.stroke();
 		
 	}
