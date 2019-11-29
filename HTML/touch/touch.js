634dd804064f15c00c84fff8fd8a451f05b58208
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2019-11-29 13:22:43 +0200

    Add files via upload

--- HTML/touch/touch.js
+++ HTML/touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML ="Logic version = 2019.11.29.1";
+document.getElementById("id_logic").innerHTML ="Logic version = 2019.11.29.2";
 window.addEventListener("touchstart",touch_start_uab);
 
 var canvas = document.getElementById("id_canvas");
@@ -6,6 +6,18 @@ var context = canvas.getContext("2d");
 
 var client_rect = canvas.getBoundingClientRact();
 
+function get_randomColor()
+{
+	var tmp ="01234566789ABCDEF";
+	var culoare = "#";
+	for (var i=0;i<6;i++)
+	{
+		culoare += tmp[Math.floor(Math.random() * 16)]
+		
+	}
+	return culoare;
+	
+}
 function touch_start_uab(e)
 {
 	var t = e.changeTouches;
@@ -13,6 +25,8 @@ function touch_start_uab(e)
 	{
 		context.beginPath();
 		context.arc(t[i].pageX - client_rect.left,t[i].pageY - client_rect.top,10,0,2*Math.PI);
+		context.fillStyle = get_randomColor();
+		context.strokeStyle = context.fillStyle;
 		context.stroke();
 		
 	}
